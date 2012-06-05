bd4b5b31b01062b4e98b029d8a784e3bc5fcd80f
Carlo Colombo <carlo.colombo@openmindonline.it>
2012-06-05 09:34:36 +0200

    mvn aliases

---
+++ custom-host/carlo-laptop/aliases.zsh
@@ -0,0 +1,3 @@
+alias mee='mvn eclipse:eclipse -DdownloadJavadocs=true -DdownloadSources=true'
+alias mci='mvn clean install -DdownloadJavadocs=true -DdownloadSources=true' 
+alias mci-skip='mvn clean install -DskipTests=true -DdownloadJavadocs=true -DdownloadSources=true'
