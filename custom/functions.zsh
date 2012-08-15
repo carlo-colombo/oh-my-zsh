4701cec0514100e34e7085503fc1b1adb0ea171f
Carlo Colombo <carlo.colombo@openmindonline.it>
2012-08-15 12:13:09 +0200

    function to mount ipad flexplayer document folder

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -2,3 +2,14 @@ function magnet
 {
     wget http://zoink.it/torrent/$1.torrent --output-document=$HOME/Dropbox/torrent/$1.torrent
 }
+
+function flexplayer()
+{           
+    FLEX_PLAYER_DIRECTORY="$HOME/ipad.flexplayer"
+
+    if [[ ! -x "$FLEX_PLAYER_DIRECTORY" ]]; then
+        mkdir "$FLEX_PLAYER_DIRECTORY"
+    fi
+
+    ifuse --appid com.personasoftware.flexplayer $FLEX_PLAYER_DIRECTORY
+}
