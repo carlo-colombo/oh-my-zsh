0c9c2b90642d8d9ba94e0f91b5930a38cbe74ca2
Carlo Colombo <carlo.colombo@openmindonline.it>
2012-09-20 16:25:25 +0200

    magnet to torrent

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -1,8 +1,5 @@
 function magnet
 {
-    #http://wiki.rtorrent.org/MagnetUri
-
-    # cd $HOME/Dropbox/torrent/ || exit    # set your watch directory here
-    # [[ "$1" =~ xt=urn:btih:([^&/]+) ]] || exit
-    # echo "d10:magnet-uri${#1}:${1}e" > "meta-${BASH_REMATCH[1]}.torrent"
+    [[ "$1" =~ xt=urn:btih:([^\&/]+) ]] || exit
+    echo "d10:magnet-uri${#1}:${1}e" > "$HOME/Dropbox/torrent/meta-${match[1]}.torrent"
 }
