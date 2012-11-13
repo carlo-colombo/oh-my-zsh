5f20960c8762bab4644faa983b0ca65278b20c35
Carlo Colombo <carlo.colombo@openmindonline.it>
2012-11-13 09:52:03 +0100

    alias to load all vbox modules

--- custom-host/carlo-laptop/aliases.zsh
+++ custom-host/carlo-laptop/aliases.zsh
@@ -3,3 +3,4 @@ alias mci='mvn clean install -DdownloadJavadocs=true -DdownloadSources=true'
 alias mci-skip='mvn clean install -DskipTests=true -DdownloadJavadocs=true -DdownloadSources=true'
 
 alias grc='export GRAILS_HOME=/home/carlo/bin/grails-2.1.0.RC2; /home/carlo/bin/grails-2.1.0.RC2/bin/grails'
+alias modall='sudo modprobe --all vboxdrv vboxnetadp vboxnetflt vboxpci'
