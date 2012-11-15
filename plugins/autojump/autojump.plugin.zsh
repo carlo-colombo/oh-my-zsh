3122f9748a9905e660d4a428a5f5da3404df6a66
Carlo Colombo <carlo.colombo@openmindonline.it>
2012-11-15 14:54:43 +0100

    latest autojump arch installation directory

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -1,6 +1,8 @@
 if [ $commands[autojump] ]; then # check if autojump is installed
   if [ -f /usr/share/autojump/autojump.zsh ]; then # debian and ubuntu package
     . /usr/share/autojump/autojump.zsh
+  elif [ -f /usr/etc/profile.d/autojump.zsh ]; then # arch
+    . /usr/etc/profile.d/autojump.zsh
   elif [ -f /etc/profile.d/autojump.zsh ]; then # manual installation
     . /etc/profile.d/autojump.zsh
   elif [ -f /opt/local/etc/profile.d/autojump.zsh ]; then # mac os x with ports
