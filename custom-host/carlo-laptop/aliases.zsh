5fb1e877d31d7a5f9ae4bf7de46f943787732c0b
Carlo Colombo <carlo.colombo@openmindonline.it>
2012-12-17 09:31:28 +0100

    vpnc aliases

--- custom-host/carlo-laptop/aliases.zsh
+++ custom-host/carlo-laptop/aliases.zsh
@@ -4,3 +4,6 @@ alias mci-skip='mvn clean install -DskipTests=true -DdownloadJavadocs=true -Ddow
 
 alias grc='export GRAILS_HOME=/home/carlo/bin/grails-2.1.0.RC2; /home/carlo/bin/grails-2.1.0.RC2/bin/grails'
 alias modall='sudo modprobe --all vboxdrv vboxnetadp vboxnetflt vboxpci'
+
+alias vpnc='sudo vpnc'
+alias vpnc-disconnect='sudo /usr/sbin/vpnc-disconnect'
