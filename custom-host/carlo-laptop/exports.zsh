1f913104a803f7edea72de2fa3490f988b6f59f6
Carlo Colombo <carlo.colombo@openmindonline.it>
2012-12-17 09:31:07 +0100

    update grails + heapspace to 1.5GB

--- custom-host/carlo-laptop/exports.zsh
+++ custom-host/carlo-laptop/exports.zsh
@@ -1,6 +1,6 @@
-export GRAILS_OPTS="-server -Xmx512M -XX:MaxPermSize=512m -Dfile.encoding=UTF-8"
+export GRAILS_OPTS="-server -Xmx1536M -XX:MaxPermSize=512m -Dfile.encoding=UTF-8"
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
-export GRAILS_HOME="/home/carlo/bin/grails-2.1.1"
+export GRAILS_HOME="/home/carlo/bin/grails-2.1.2"
 export GROOVY_HOME="/home/carlo/bin/groovy-1.8.8"
 
 export JAVA_OPTS="-XX:MaxPermSize=512M"
