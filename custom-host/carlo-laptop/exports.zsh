226ac132832dee05b17bfa63d22a93386dbbf503
Carlo Colombo <carlo.colombo@openmindonline.it>
2013-02-12 11:32:51 +0100

    JAVA_OPTS, SVN_MERGE

--- custom-host/carlo-laptop/exports.zsh
+++ custom-host/carlo-laptop/exports.zsh
@@ -3,5 +3,7 @@ export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
 export GRAILS_HOME="/home/carlo/bin/grails-2.1.2"
 export GROOVY_HOME="/home/carlo/bin/groovy-1.8.8"
 
-export JAVA_OPTS="-XX:MaxPermSize=512M"
+export JAVA_OPTS="-Xmx2000M -XX:MaxPermSize=256M"
 export CATALINA_OPTS="-XX:MaxPermSize=256M"
+
+export SVN_MERGE="kdiff3"
