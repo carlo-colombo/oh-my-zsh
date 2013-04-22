585a50bb33f56bf1d5a9b831d978c9df88613cbe
Carlo Colombo <carlo.colombo@gmx.com>
2013-04-23 00:24:03 +0200

    git dropbox init (with remote)

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -3,3 +3,26 @@ function magnet
     [[ "$1" =~ xt=urn:btih:([^\&/]+) ]] || exit
     echo "d10:magnet-uri${#1}:${1}e" > "$HOME/Dropbox/torrent/meta-${match[1]}.torrent"
 }
+
+function create_bare_repo
+{
+    repo=$HOME/Dropbox/repos/$1.git
+    mkdir $repo
+    git init --bare $repo
+    unset repo
+}
+
+function set_dropbox_remote
+{
+    repo="$HOME/Dropbox/repos/$1.git"
+    git remote add "${2:-origin}" "file://$repo"
+    unset repo
+}
+
+function git_dropbox_init
+{
+    repo_name=$(pwd | xargs basename)
+    git init
+    create_bare_repo   $repo_name
+    set_dropbox_remote $repo_name "${1:-origin}"
+}
