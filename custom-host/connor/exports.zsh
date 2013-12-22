392a3c055cbd047a053e55f7f9deec9c48301d6b
Carlo Colombo <carlo.colombo@gmx.com>
2013-12-22 20:43:52 +0100

    removed some HOME

--- custom-host/connor/exports.zsh
+++ custom-host/connor/exports.zsh
@@ -1,7 +1,5 @@
 export GRAILS_OPTS="-server -Xmx1536M -XX:MaxPermSize=512m -Dfile.encoding=UTF-8"
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
-export GRAILS_HOME="/home/carlo/bin/grails-2.1.2"
-export GROOVY_HOME="/home/carlo/bin/groovy-1.8.8"
 
 export JAVA_OPTS="-Xmx2000M -XX:MaxPermSize=256M"
 export CATALINA_OPTS="-XX:MaxPermSize=256M"
